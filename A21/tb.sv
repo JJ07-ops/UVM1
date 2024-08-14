8ec8489ddf5ac0254eb5f8539af5163386f9be66
Sim Jia Jun <118655979+JJ07-ops@users.noreply.github.com>
2024-08-14 20:23:45 +0800

    Update tb.sv

--- A21/tb.sv
+++ A21/tb.sv
@@ -1,6 +1,7 @@
 /*Assignment agenda:
 
-Create a class "my_object" by extending the UVM_OBJECT class. Add three logic datatype datamembers "a", "b", and "c" with sizes of 2, 4, and 8 respectively. Generate a random value for all the data members and send the values of the variables to the console by using the print method.
+Create a class "my_object" by extending the UVM_OBJECT class. Add three logic datatype datamembers "a", "b", and "c" with sizes of 2, 4, and 8 respectively. 
+Generate a random value for all the data members and send the values of the variables to the console by using the print method.
 
 Use Automation for sending variable values to console.
 
