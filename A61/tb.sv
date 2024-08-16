cb26ea7622794c90accfc40673c2b99791367851
Sim Jia Jun <118655979+JJ07-ops@users.noreply.github.com>
2024-08-16 10:55:10 +0800

    Update tb.sv

--- A61/tb.sv
+++ A61/tb.sv
@@ -6,8 +6,6 @@ Add End of elaboration phase in Driver, Monitor, Environment and Test Class.
 Driver and Monitor are child to Environment while Environment is child to Test Class. 
 Build Heirarchy and perform execution of the code to verify End of ELaboration phase executes in Bottom-Up fashion. Use template mentioned below.
 
-
-Student's note: Assignment name was A51 but the student changed it to A61 due to the assignment being in Section 6 of the course.
 */
 
 `timescale 1ns/1ps
